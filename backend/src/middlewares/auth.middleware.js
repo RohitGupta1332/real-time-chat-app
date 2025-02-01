7555c29af7c961e39c5f0e6c0bc86503eca36057
Rohit <rohitgupta7471@gmail.com>
2025-02-01 15:38:26 +0530

    Added email verification

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const protectRoute = (req, res, next) => {
+    try{
+        const token = req.cookies.token;
+        if(!token){
+            return res.status(401).json({message: "Unauthorised token"});
+        }
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.user = decoded;
+        next();
+    }
+    catch(error){
+        return res.status(401).json({message: "Error in token verification ", error: error})
+    }
+}
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,23 +1,24 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    isVerified: {
-        type: Boolean,
-        default: false
-    },
-    verificationCode: {
-        type: String,
-        default: null
-    }
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationCode: {
+    type: String,
+    default: null,
+  },
+  verficationTokenExpiresAt: Date,
+});
 
 export const User = mongoose.model("user", userSchema);
