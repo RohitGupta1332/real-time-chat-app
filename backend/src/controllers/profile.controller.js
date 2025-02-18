2de0f86c4e4c9afc81a4eac9fa9dc17c9c0415db
Rohit <rohitgupta7471@gmail.com>
2025-02-18 20:00:35 +0530

    Created update profile and message routes

--- backend/src/controllers/profile.controller.js
+++ backend/src/controllers/profile.controller.js
@@ -4,9 +4,9 @@ import cloudinary from "cloudinary";
 export const createProfile = async (req, res) => {
     try {
         let { name, username, gender, image, bio, instagramUrl, youtubeUrl, facebookUrl, twitterUrl } = req.body;
-        const existingUser = await Profile.findOne({username});
-        if(existingUser){
-            return res.status(400).json({message: "Username already exists"});
+        const existingUser = await Profile.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: "Username already exists" });
         }
         let imageUrl;
         if (image) {
@@ -27,6 +27,41 @@ export const createProfile = async (req, res) => {
         });
 
         res.status(201).json({ message: "Profile created successfully" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error: error.message || error });
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        let { name, username, gender, image, bio, instagramUrl, youtubeUrl, facebookUrl, twitterUrl } = req.body;
+        const existingUser = await Profile.findOne({ username });
+        if (!existingUser) {
+            return res.status(404).json({ message: "Profile not found" });
+        }
+
+        let imageUrl = existingUser.image;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        await Profile.updateOne({ username },
+            {
+                $set: {
+                    name,
+                    gender,
+                    image: imageUrl,
+                    bio,
+                    instagramUrl,
+                    youtubeUrl,
+                    facebookUrl,
+                    twitterUrl
+                }
+            }
+        );
+        res.status(200).json({ message: "Profile updated successfully" });
 
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error.message || error });
--- backend/src/routes/profile.route.js
+++ backend/src/routes/profile.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import {createProfile} from "../controllers/profile.controller.js";
+import {createProfile, updateProfile} from "../controllers/profile.controller.js";
 
 const router = express.Router();
 
 router.post("/create",protectRoute, createProfile);
+router.post("/update", protectRoute, updateProfile);
 
 export default router;
