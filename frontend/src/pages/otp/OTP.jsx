716ca8c248a802e03e088720cda02153f1494a2f
Rohit <rohitgupta7471@gmail.com>
2025-02-27 20:24:51 +0530

    Created OTP page

--- frontend/src/pages/otp/OTP.jsx
+++ frontend/src/pages/otp/OTP.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuthStore } from "../../store/useAuthStore";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
-function OTP (){
+function OTP() {
+    const [otp, setOtp] = useState("");
+    const { verifyEmail } = useAuthStore();
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        console.log(otp)
+        if (otp.length === 6) {
+            verifyEmail(otp, navigate);
+        }
+        else {
+            toast.error("Invalid OTP");
+        }
+    }
+
     return (
-        <div>OTP</div>
+        <div>
+            <h1>OTP Verification</h1>
+            <input type="text" name="otp" id="" placeholder="Enter OTP" onChange={(e) => setOtp(e.target.value)} />
+            <input type="submit" value="Submit" onClick={handleSubmit} />
+            <ToastContainer />
+        </div>
     )
 }
 
