ae872456d0df72ef6f6d69d1e0d62585b761351e
Sajal <schitlangia26@gmail.com>
2025-04-12 14:14:07 +0530

    Toast-Added

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/button.module.css'
 
-const Button = ({text}) => {
-    return <button className={styles.button}>{text}</button>
+const Button = ({text, disabled = false}) => {
+    return <button className={styles.button} disabled={disabled}>{text}</button>
 }
 
 export default Button;
