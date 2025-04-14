b99482c970afea1771105ad45f3818d3cb89c26c
Rohit <rohitgupta7471@gmail.com>
2025-04-14 13:01:35 +0530

    added view profile

--- backend/src/routes/profile.route.js
+++ backend/src/routes/profile.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { createProfile, updateProfile } from "../controllers/profile.controller.js";
-import { upload } from "../utils/multer.js";
+import { createProfile, updateProfile, getProfile } from "../controllers/profile.controller.js";
+
 
 const router = express.Router();
 
-router.post("/create",protectRoute, createProfile);
+router.post("/create", protectRoute, createProfile);
 router.post("/update", protectRoute, updateProfile);
+router.get("/profile", protectRoute, getProfile);
 
 export default router;
